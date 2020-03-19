67b58f1fe4de3815aeba55cc4103c6971f907415
Santiago Argilla <sargilla@gmail.com>
2020-03-19 19:39:06 -0300

    Leo archivo profes.json y lo parseo

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ console.log('Ya sacamos nuestra primera foto')
 
 
 console.log('me re olvide')
+
+const fs = require('fs');
+
+let profes = JSON.parse(fs.readFileSync('profes.json','utf-8'));
+console.log(profes);
